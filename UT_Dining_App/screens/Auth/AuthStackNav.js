8ca0bc56f3dfebda4ba24eff25d5295c1d07ed56
jzhao20 <62364843+jzhao20@users.noreply.github.com>
2020-12-25 20:35:04 -0600

    random garbage

--- UT_Dining_App/screens/Auth/AuthStackNav.js
+++ UT_Dining_App/screens/Auth/AuthStackNav.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Login from '../Login'
+import Login from '../Login';
+import Signup from '../Signup';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 const AuthStackNav = () => {
   return (
     <Stack.Navigator screenOptions={{ header: () => null }}>
-      { <Stack.Screen name="Login" component={Login} />
-      /*<Stack.Screen name="Signup" component={Signup} />
-  <Stack.Screen name="Setup" component={Setup} /> */}
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+  {/* <Stack.Screen name="Setup" component={Setup} />  */}
     </Stack.Navigator>
   );
 };
---
+++ UT_Dining_App/screens/Signup/index.js
@@ -0,0 +1,19 @@
+import React, { useState, useContext} from 'react';
+import {AuthContext} from '../App';
+import {TextInput} from 'react-native-gesture-handler';
+export default function Login({navigation}){
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const{setUser} = useContext(AuthContext);
+
+    const submitHandler = () =>{
+        signup(email, password)
+        .then((res)=>{
+            navigation.navigate("Setup", {uid:res.user.uid});
+        })
+        .catch((err)=>{
+            alert(err)
+        });
+
+    }
+}
