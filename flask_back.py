dcd9a067631cae153b47d1aac9b542e8294b80df
jzhao20 <jzhaotxus@gmail.com>
2021-01-06 18:42:54 -0600

    fixed up the get halls so the logic would work even if it needs to be corrected

--- flask_back.py
+++ flask_back.py
@@ -1,6 +1,7 @@
 import os
 import scrape
 import image_classification
+import json
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from datetime import datetime, date
@@ -12,8 +13,20 @@ mongo = PyMongo(app)
 
 @app.route('/halls/get', methods = ['GET'])
 def get_halls():
-    #return a jsonified array
-    return jsonify(scrape.get_dining_halls)
+	if request.args["force"] == "True":
+		val = json.dumps(scrape.get_dining_halls())
+		database = mongo.db.dining_halls
+		database_to_edit = database.find()
+		try:
+			database.update_one(database_to_edit[0], {"$set":{"dining_halls":val}})
+		except:
+			print("here")
+			database.insert_one({"dining_halls":val})
+		return val
+	else:
+		database = mongo.db.dining_halls
+		return database.find()[0]["dining_halls"]
+
 
 def update_database(date_specified, dining_hall, meal_time):
     date_adjusted = datetime.strptime(date_specified, "%m/%d/%Y")
